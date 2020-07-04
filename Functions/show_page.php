45216579fc184d834570f0ec34c068d09d30c047
ljq0306 <ljq0036@gmail.com>
2020-07-04 14:25:47 +0800

    Improve page appearance.

--- Functions/show_page.php
+++ Functions/show_page.php
@@ -12,34 +12,42 @@
                 print '<p>last edited time : ' . $row['last_edit_time'] . '<form action="edit.php" method="post">
 <input type="hidden" name="page" value="'.$page.'">
 <input type="submit" value="Edit" name="edit"><input type="submit" value="Delete" name="del">'."</form></p><p>";
+
                 for ($i = 1; $i < 180; $i++) {
                     print '-';
                 }
                 print '</p></div>';
+//                if ($row['son']!=''){
+//                    echo '<ol>';//begin of page list
+//                }
+                echo '<ol>';//content list start
                 break;
             case 'h2':
-                print '<h2>' . $row['contents'] . '</h2>';
+                echo '<li class="h2_li"><span><b>'. $row['contents'] .'</b></span><br>';
+//                print '<h2>' . $row['contents'] . '</h2>';
+                echo '<ul class="h2_ol">';
                 break;
             case 'h3':
-                print '<h3>' . $row['contents'] . '</h3>';
+                print '<li class="content_li"><h3>' . $row['contents'] . '</h3></li>';
                 break;
             case 'text':
-                print '<p><pre>'.$row['contents'].'</pre></p>';
+                print '<li class="content_li"><p style="text-indent: 2em;"><pre>'.$row['contents'].'</pre></p></li>';
                 break;
             case "code":
-                print '<div class="code">';
-////                auto split into lines by delimiter
-//                $lines = explode(';', $row['contents']);
-//                print '<ul>';
-//                foreach ($lines as $key => $val) {
-//                    print '<li>' . $val . '</li>';
-//                }
-//                print '</ul></div>';
-                echo '<code>'.$row['contents'].'</code>';
-                echo '</div>';
+                print '<li class="content_li"><div class="code">';
+//                auto split into lines by delimiter
+                $row['contents']=preg_replace(array('/\r\n/', '/\n/'), '', $row['contents']);//replace all switch line symbol
+                $lines = explode('<br />', $row['contents']);
+                print '<ol>';
+                foreach ($lines as $key => $val) {
+                    print '<li>' . $val . '</li>';
+                }
+                print '</ol></div>';
+//                echo '<code>'.$row['contents'].'</code>';
+                echo '</li>';
                 break;
             case 'graph':
-                echo "<img class='contents_img' src={$row['contents']}>";
+                echo "<li class=\"content_li\"><img class='contents_img' src={$row['contents']}></li>";
                 break;
         }
     }
@@ -55,10 +63,20 @@
             //if node has son_node,use index(son_node)
             if ($row['son']) {
                 $sons = explode(',', $row['son']);
+//                sort($sons);
                 foreach ($sons as $key => $val) {
                     index($val, $page);
                 }
+//                return 1;       //traveled all sons
             }
+            if ($row['flag']=='h1'){
+                //page title, whole page printed
+                echo '</ol>';
+            }elseif ($row['flag']=='h2'){
+                //end of a subtitle
+                echo '</ul></li><br>';
+            }
+//            return 0;       //node have no son
         }
         $page=str_ireplace('\'', '', $page);//table name in ``
         $res=search('page', $page);
